db0547aa2bf79854139710e3e53e47cbae53a131
Okko Ailio <oiaili@utu.fi>
2024-08-05 14:52:18 +0300

    Minor adjustments in clickable sizes

--- packages/frontend/src/components/toolbar/SearchFilter.jsx
+++ packages/frontend/src/components/toolbar/SearchFilter.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import '../../css/searchFilter.scss'
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchFilter = ({ searchInput, setSearchInput }) => {
+  const inputRef = useRef(null);
 
   function clearSearch(){
     setSearchInput('')
@@ -16,11 +18,17 @@ const SearchFilter = ({ searchInput, setSearchInput }) => {
     setSearchInput(e.target.value);
   }
 
+  const focusInputField = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div className="search-filter">
-      <button className='icon-container'><FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' /></button>
+      <button className='icon-container' onClick={focusInputField}><FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' /></button>
       <div className='search-field-container'>
-        <input className='search-input' type="text" value={searchInput} onChange={handleSearchChange} placeholder="Food name..."/>
+        <input ref={inputRef} className='search-input' type="text" value={searchInput} onChange={handleSearchChange} placeholder="Food name..."/>
         {searchInput && (
           <button className='icon-container' onClick={clearSearch}><FontAwesomeIcon icon={faXmark} className='x-icon' /></button>
         )}
