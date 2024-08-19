cdc04ebedbb780875f11a644a440c65a15868839
Okko Ailio <oiaili@utu.fi>
2024-08-19 12:34:06 +0300

    Added styles for the date filter tag

--- packages/frontend/src/components/toolbar/ActiveFilterTags.jsx
+++ packages/frontend/src/components/toolbar/ActiveFilterTags.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import RenderTags from "./RenderTags";
 import formatDate from "../../utils/formatDate";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import '../../css/activeFilterTags.scss'
+import '../../css/tag.scss'
 
 const ActiveFilterTags = ({ proteinFilters, setProteinFilters, carbFilters, setCarbFilters, 
                             dateFilter, setDateFilter, dateFilterType, clearFilters}) => {
@@ -23,7 +26,10 @@ const ActiveFilterTags = ({ proteinFilters, setProteinFilters, carbFilters, setC
           filters={carbFilters}
           setFilters={setCarbFilters}
         />
-        {dateFilter && <button onClick={()=>setDateFilter("")}>{dateFilterType} {formatDate(dateFilter)} x</button>}
+        {dateFilter && 
+          <button className="filter-tag" onClick={()=>setDateFilter("")}>
+            {dateFilterType} {formatDate(dateFilter)} <FontAwesomeIcon icon={faCircleXmark} className='filter-tag-icon'/>
+          </button>}
         <button className="active-filter-clear-button" onClick={clearFilters}>Clear all</button>
       </div>
     </div>
